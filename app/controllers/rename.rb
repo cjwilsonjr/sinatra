41131f5ca5bcef190cdea1bc8283ea4e40ee0c23
CJ Wilson <christopher.wilson.jr@gmail.com>
2015-04-20 22:40:44 -0500

    add rename controller

---
+++ app/controllers/rename.rb
@@ -0,0 +1,46 @@
+#INDEX
+get "/renames" do
+  @renames = Rename.all
+  erb :"renames/index"
+end
+
+#NEW
+get "/renames/new" do
+  @rename = Rename.new
+  erb :"renames/new"
+end
+
+#CREATE
+post "/renames" do
+  @rename = Rename.new(params[:rename])
+  if @rename.save
+    redirect "/renames/#{@rename.id}"
+  else
+    @errors = @rename.errors.full_messages
+end
+
+#SHOW
+get "/renames/:id" do
+  @rename = Rename.find(params[:id])
+  erb :"/renames/show"
+end
+
+#EDIT
+get "/renames/:id/edit" do
+  @rename = Rename.find(params[:id])
+  erb :"/renames/edit"
+end
+
+#UPDATE
+put "/renames/:id" do
+  @rename = Rename.find(params[:id])
+  @rename.update!(params[:rename]
+  redirect "/renames/#{params[:id]}"
+end
+
+#DELETE
+delete "/renames/:id" do
+  @rename = Rename.find(params[:id])
+  @rename.destroy
+  redirect "/renames"
+end
